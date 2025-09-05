c332d0d074239ac8b376218f610debe8f250d3a6
DrPlop <thibault.patois@spendesk.com>
2025-09-05 17:08:59 +0200

    feat(Tailwind): support tailwind 4

--- src/colors.ts
+++ src/colors.ts
@@ -1,98 +1,107 @@
 export const colors = {
-  backgroundBackdrop: 'var(--color-backdrop)',
-  backgroundPrimaryDefault: 'var(--color-background-primary-default)',
-  backgroundPrimaryHover: 'var(--color-background-primary-hover)',
-  backgroundPrimaryPressed: 'var(--color-background-primary-pressed)',
-  backgroundPrimarySelected: 'var(--color-background-primary-selected)',
-  backgroundPrimaryDisabled: 'var(--color-background-primary-disabled)',
-  backgroundPrimaryReadonly: 'var(--color-background-primary-readonly)',
-  backgroundSecondaryDefault: 'var(--color-background-secondary-default)',
-  backgroundSecondaryHover: 'var(--color-background-secondary-hover)',
+  backgroundBackdrop: 'var(--grapes-color-backdrop)',
+  backgroundPrimaryDefault: 'var(--grapes-color-background-primary-default)',
+  backgroundPrimaryHover: 'var(--grapes-color-background-primary-hover)',
+  backgroundPrimaryPressed: 'var(--grapes-color-background-primary-pressed)',
+  backgroundPrimarySelected: 'var(--grapes-color-background-primary-selected)',
+  backgroundPrimaryDisabled: 'var(--grapes-color-background-primary-disabled)',
+  backgroundPrimaryReadonly: 'var(--grapes-color-background-primary-readonly)',
+  backgroundSecondaryDefault:
+    'var(--grapes-color-background-secondary-default)',
+  backgroundSecondaryHover: 'var(--grapes-color-background-secondary-hover)',
 
-  backgroundTertiaryDefault: 'var(--color-background-tertiary-default)',
+  backgroundTertiaryDefault: 'var(--grapes-color-background-tertiary-default)',
   backgroundComplementaryDefault:
-    'var(--color-background-complementary-default)',
-  backgroundComplementaryHover: 'var(--color-background-complementary-hover)',
+    'var(--grapes-color-background-complementary-default)',
+  backgroundComplementaryHover:
+    'var(--grapes-color-background-complementary-hover)',
   backgroundComplementaryPressed:
-    'var(--color-background-complementary-pressed)',
+    'var(--grapes-color-background-complementary-pressed)',
   backgroundComplementaryDisabled:
-    'var(--color-background-complementary-disabled)',
+    'var(--grapes-color-background-complementary-disabled)',
   backgroundPrimaryBrandDefault:
-    'var(--color-background-primary-brand-default)',
-  backgroundPrimaryBrandHover: 'var(--color-background-primary-brand-hover)',
+    'var(--grapes-color-background-primary-brand-default)',
+  backgroundPrimaryBrandHover:
+    'var(--grapes-color-background-primary-brand-hover)',
   backgroundPrimaryBrandPressed:
-    'var(--color-background-primary-brand-pressed)',
-  backgroundPrimaryInfoDefault: 'var(--color-background-primary-info-default)',
-  backgroundPrimaryInfoHover: 'var(--color-background-primary-info-hover)',
-  backgroundPrimaryInfoPressed: 'var(--color-background-primary-info-pressed)',
+    'var(--grapes-color-background-primary-brand-pressed)',
+  backgroundPrimaryInfoDefault:
+    'var(--grapes-color-background-primary-info-default)',
+  backgroundPrimaryInfoHover:
+    'var(--grapes-color-background-primary-info-hover)',
+  backgroundPrimaryInfoPressed:
+    'var(--grapes-color-background-primary-info-pressed)',
   backgroundPrimarySuccessDefault:
-    'var(--color-background-primary-success-default)',
+    'var(--grapes-color-background-primary-success-default)',
   backgroundPrimarySuccessHover:
-    'var(--color-background-primary-success-hover)',
+    'var(--grapes-color-background-primary-success-hover)',
   backgroundPrimarySuccessPressed:
-    'var(--color-background-primary-success-pressed)',
+    'var(--grapes-color-background-primary-success-pressed)',
   backgroundPrimaryWarningDefault:
-    'var(--color-background-primary-warning-default)',
+    'var(--grapes-color-background-primary-warning-default)',
   backgroundPrimaryWarningHover:
-    'var(--color-background-primary-warning-hover)',
+    'var(--grapes-color-background-primary-warning-hover)',
   backgroundPrimaryWarningPressed:
-    'var(--color-background-primary-warning-pressed)',
+    'var(--grapes-color-background-primary-warning-pressed)',
   backgroundPrimaryAlertDefault:
-    'var(--color-background-primary-alert-default)',
-  backgroundPrimaryAlertHover: 'var(--color-background-primary-alert-hover)',
+    'var(--grapes-color-background-primary-alert-default)',
+  backgroundPrimaryAlertHover:
+    'var(--grapes-color-background-primary-alert-hover)',
   backgroundPrimaryAlertPressed:
-    'var(--color-background-primary-alert-pressed)',
+    'var(--grapes-color-background-primary-alert-pressed)',
   backgroundSecondaryBrandDefault:
-    'var(--color-background-secondary-brand-default)',
+    'var(--grapes-color-background-secondary-brand-default)',
   backgroundSecondaryBrandHover:
-    'var(--color-background-secondary-brand-hover)',
+    'var(--grapes-color-background-secondary-brand-hover)',
   backgroundSecondaryBrandPressed:
-    'var(--color-background-secondary-brand-pressed)',
+    'var(--grapes-color-background-secondary-brand-pressed)',
   backgroundSecondaryInfoDefault:
-    'var(--color-background-secondary-info-default)',
-  backgroundSecondaryInfoHover: 'var(--color-background-secondary-info-hover)',
+    'var(--grapes-color-background-secondary-info-default)',
+  backgroundSecondaryInfoHover:
+    'var(--grapes-color-background-secondary-info-hover)',
   backgroundSecondaryInfoPressed:
-    'var(--color-background-secondary-info-pressed)',
+    'var(--grapes-color-background-secondary-info-pressed)',
   backgroundSecondarySuccessDefault:
-    'var(--color-background-secondary-success-default)',
+    'var(--grapes-color-background-secondary-success-default)',
   backgroundSecondarySuccessHover:
-    'var(--color-background-secondary-success-hover)',
+    'var(--grapes-color-background-secondary-success-hover)',
   backgroundSecondarySuccessPressed:
-    'var(--color-background-secondary-success-pressed)',
+    'var(--grapes-color-background-secondary-success-pressed)',
   backgroundSecondaryWarningDefault:
-    'var(--color-background-secondary-warning-default)',
+    'var(--grapes-color-background-secondary-warning-default)',
   backgroundSecondaryWarningHover:
-    'var(--color-background-secondary-warning-hover)',
+    'var(--grapes-color-background-secondary-warning-hover)',
   backgroundSecondaryWarningPressed:
-    'var(--color-background-secondary-warning-pressed)',
+    'var(--grapes-color-background-secondary-warning-pressed)',
   backgroundSecondaryAlertDefault:
-    'var(--color-background-secondary-alert-default)',
+    'var(--grapes-color-background-secondary-alert-default)',
   backgroundSecondaryAlertHover:
-    'var(--color-background-secondary-alert-hover)',
+    'var(--grapes-color-background-secondary-alert-hover)',
   backgroundSecondaryAlertPressed:
-    'var(--color-background-secondary-alert-pressed)',
+    'var(--grapes-color-background-secondary-alert-pressed)',
 
-  contentPrimary: 'var(--color-content-primary)',
-  contentSecondaryBgPrimary: 'var(--color-content-secondary-bg-primary)',
-  contentSecondaryBgSecondary: 'var(--color-content-secondary-bg-secondary)',
-  contentDecorativeIcon: 'var(--color-content-decorative-icon)',
-  contentComplementary: 'var(--color-content-complementary)',
-  contentSelected: 'var(--color-content-selected)',
-  contentDisabled: 'var(--color-content-disabled)',
-  contentBrandDefault: 'var(--color-content-brand-default)',
-  contentBrandHover: 'var(--color-content-brand-hover)',
-  contentBrandPressed: 'var(--color-content-brand-pressed)',
-  contentInfoDefault: 'var(--color-content-info-default)',
-  contentSuccessDefault: 'var(--color-content-success-default)',
-  contentWarningDefault: 'var(--color-content-warning-default)',
-  contentAlertDefault: 'var(--color-content-alert-default)',
+  contentPrimary: 'var(--grapes-color-content-primary)',
+  contentSecondaryBgPrimary: 'var(--grapes-color-content-secondary-bg-primary)',
+  contentSecondaryBgSecondary:
+    'var(--grapes-color-content-secondary-bg-secondary)',
+  contentDecorativeIcon: 'var(--grapes-color-content-decorative-icon)',
+  contentComplementary: 'var(--grapes-color-content-complementary)',
+  contentSelected: 'var(--grapes-color-content-selected)',
+  contentDisabled: 'var(--grapes-color-content-disabled)',
+  contentBrandDefault: 'var(--grapes-color-content-brand-default)',
+  contentBrandHover: 'var(--grapes-color-content-brand-hover)',
+  contentBrandPressed: 'var(--grapes-color-content-brand-pressed)',
+  contentInfoDefault: 'var(--grapes-color-content-info-default)',
+  contentSuccessDefault: 'var(--grapes-color-content-success-default)',
+  contentWarningDefault: 'var(--grapes-color-content-warning-default)',
+  contentAlertDefault: 'var(--grapes-color-content-alert-default)',
 
-  borderDefault: 'var(--color-border-default)',
-  borderComplementary: 'var(--color-border-complementary)',
-  borderHover: 'var(--color-border-hover)',
-  borderSelected: 'var(--color-border-selected)',
-  borderInfo: 'var(--color-border-info)',
-  borderSuccess: 'var(--color-border-success)',
-  borderWarning: 'var(--color-border-warning)',
-  borderAlert: 'var(--color-border-alert)',
+  borderDefault: 'var(--grapes-color-border-default)',
+  borderComplementary: 'var(--grapes-color-border-complementary)',
+  borderHover: 'var(--grapes-color-border-hover)',
+  borderSelected: 'var(--grapes-color-border-selected)',
+  borderInfo: 'var(--grapes-color-border-info)',
+  borderSuccess: 'var(--grapes-color-border-success)',
+  borderWarning: 'var(--grapes-color-border-warning)',
+  borderAlert: 'var(--grapes-color-border-alert)',
 };
